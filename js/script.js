8fcf6825de135d42c2eca028f116755c734500df
tomstuder <tom@tomstuder.com>
2016-05-02 20:25:41 -0400

    init commit files setup content added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Variables
+
+    var score;
+    var totalScore;
+    var lives;
+    var livesLeft;
+    var number;
+    var randomNumber;
+
+// Functions
+
+    // Random Number Generator
+    // Button Hi
+    // Button Lo
