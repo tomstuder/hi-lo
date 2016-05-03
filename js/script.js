6c6e3d1a88360d34d635550d42725a72aa1a1d28
tomstuder <tom@tomstuder.com>
2016-05-02 23:16:22 -0400

    edited style and js file for counting lives and score

--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
 // Variables
 
     var score = 0;
-    var totalScore = 0;
-    var lives = 3;
-    var livesLeft;
+    var totalScore = score;
+    var lives = 6;
+    var livesLeft = lives;
+
+
 
 // Random Number Generator
     var randomNumber = Math.floor((Math.random() * 20) + 1);
 
 // Button Hi
-// Button Lo
+  function hi() {
+     if (randomNumber > 10) {
+        document.getElementById("modal-win").className = "display";
+        // Add Points
+        totalScore = score+=100;
+        document.getElementById("points").innerHTML = score;
+  } else {
+    document.getElementById("modal-lose").className = "display";
+     livesLeft = lives--;
+    document.getElementById("lives").innerHTML = livesLeft;
+  }
+}
+
+
+// Button Low
+
+function low() {
+   if (randomNumber < 10) {
+      document.getElementById("modal-win").className = "display";
+      // Add Points
+      totalScore = score+=100;
+      document.getElementById("points").innerHTML = score;
+    } else {
+    document.getElementById("modal-lose").className = "display";
+      livesLeft = lives--;
+    document.getElementById("lives").innerHTML = livesLeft;
+  }
+}
+
+document.getElementById("lives").innerHTML = livesLeft;
+document.getElementById("points").innerHTML = score;
 
 // Testing
     console.log(randomNumber);
